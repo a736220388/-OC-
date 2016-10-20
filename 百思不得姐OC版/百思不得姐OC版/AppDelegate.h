646eae63536167f9c6716c92f210ba3d9a421634
zwz <zwz@zwzdeiMac.local>
2016-10-20 04:11:13 -0700

    初始化

---
+++ 百思不得姐OC版/百思不得姐OC版/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐OC版
+//
+//  Created by zwz on 2016/10/20.
+//  Copyright © 2016年 zwz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思不得姐OC版/百思不得姐OC版/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思不得姐OC版
+//
+//  Created by zwz on 2016/10/20.
+//  Copyright © 2016年 zwz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
